36213d76cc7027fe3f3273989248f9d39677d96a
Baday19 <1556330234@qq.com>
2023-04-28 23:22:49 +0800

    feat: config

---
+++ src/utils/toast.js
@@ -0,0 +1,7 @@
+
+const showToast = (content, time) => {
+  const body = document.getElementsByTagName('body')[0]
+  const toastWrap = document.createElement('div')
+  const toastBox = document.createElement('div')
+  toastBox.add
+}
